453430e468b11c090a06c7f15a9680f2f720e6f7
Pedro Ivan <pedroivanlarios@gmail.com>
2023-06-28 17:33:07 -0600

    cambios vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://fluffy1720.github.io/formation-plan-frontend-prod/dual-branch/',
+  base: '/',
 });
