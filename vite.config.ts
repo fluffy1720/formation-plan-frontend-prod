9dbe973f72a0273837427215e9eb3bdd1bf5aa73
Pedro Ivan <pedroivanlarios@gmail.com>
2023-06-27 20:05:00 -0600

    cambios deploy dual2

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/formation-plan-frontend-prod/',
-  build: {
-    outDir: 'public'
-  }
+  base: 'https://fluffy1720/formation-plan-frontend-prod',
 })
